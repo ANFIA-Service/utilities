9d3601138a56350ea95d02147f8a1f049cd4c1fb
Cosimo Di Martino <derto.dimartino@gmail.com>
2025-11-24 15:45:47 +0100

    Added polars function.

--- utilities/__init__.py
+++ utilities/__init__.py
@@ -1,4 +1,8 @@
-from .verify import verify_df_pairs
+from .verify import verify_df_pairs, verify_df_pairs_polars
 from .login import build_connection_string
 
-__all__ = ["verify_df_pairs", "build_connection_string"]
+__all__ = [
+    "verify_df_pairs",
+    "verify_df_pairs_polars",
+    "build_connection_string",
+]
